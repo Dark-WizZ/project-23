b6080c56c06fc12550cb699751d3ba65b142fe99
Dark-WizZ <Dark-WizZ>
2023-04-16 00:45:34 +0530

    Before npm run eject

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,22 @@
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import { useState } from 'react'
+import {signInWithEmailAndPassword} from 'firebase/auth'
+import {auth} from '../firebase'
+
 
 function Login(props){
+  const [err, setErr] = useState(false)
+  const nav = useNavigate()
+  const submitHandler = (e) => {
+    e.preventDefault()
+    
+  }
+
   return <div className="container">
     <div className="wrapper">
       <span className="logo">Restro App</span>
       <span className="title">Chef's Login</span>
-      <form>
+      <form onSubmit={submitHandler}>
         <input required type="email" placeholder="Email" />
         <input required type="password" placeholder="Password"/>
         <button>Sign in</button>
